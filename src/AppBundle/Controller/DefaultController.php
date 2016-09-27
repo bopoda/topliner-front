c6579058f7ad4443bb87c86994ab1d7dd7c4e315
Eugene Yurkevich <eugeney@playtika.com>
2016-09-27 18:38:49 +0300

    add my first action - home page

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -9,13 +9,27 @@ use Symfony\Component\HttpFoundation\Request;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/old", name="homepage_old")
      */
-    public function indexAction(Request $request)
+    public function indexOldAction(Request $request)
     {
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
         ]);
+    }
+
+    /**
+     * Это будет вывод главной страницы.
+     *
+     * @Route("/", name="homepage")
+     */
+    public function indexAction()
+    {
+        $number = mt_rand(0, 100);
+
+        return $this->render('default/homepage.html.twig', array(
+            'number' => $number,
+        ));
     }
 }
