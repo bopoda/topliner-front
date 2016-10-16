5496d71a3e7a4cafe1ca2ba4bad23691b5fd6f14
Eugene Yurkevich <yurkevich@vicman.net>
2016-10-16 19:51:19 +0300

    add user login template

---
+++ src/AppBundle/Controller/UserController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class UserController extends Controller
+{
+    /**
+     * @Route ("/login", name="login")
+     */
+    public function loginAction()
+    {
+        return $this->render('user/login.html.twig', array(
+
+        ));
+    }
+}
