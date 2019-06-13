f517fec8acc99f6d3454b0d987ebd0be8e2dc7f9
Yauheni Yurkevich <Yauheni.Yurkevich@secretsales.com>
2019-06-13 14:17:34 +0300

    #

--- jenkins.sh
+++ jenkins.sh
@@ -12,15 +12,15 @@ set -e
     --report-file=phpcs.checkstyle.xml \
     src/AppBundle
 
-./vendor/bin/phpcs \
-    --report=checkstyle \
-    --report-file=findbugs.xml \
-    src/AppBundle
+#./vendor/bin/phpcs \
+#    --report=checkstyle \
+#    --report-file=findbugs.xml \
+#   src/AppBundle
 
-./vendor/bin/phpcs \
-    --report=checkstyle \
-    --report-file=findbugs/phpcs.checkstyle.xml \
-    src/AppBundle
+#./vendor/bin/phpcs \
+#    --report=checkstyle \
+#    --report-file=findbugs/phpcs.checkstyle.xml \
+#    src/AppBundle
 
 ./vendor/bin/phpcs --standard=PSR2 src/AppBundle/
 
